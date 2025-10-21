888260859026d3eea9bc7864a762abcdff3ecd55
John <mceddie@colostate.edu>
2025-10-21 10:09:18 -0600

    feat: get median and prs past the median

--- mine.py
+++ mine.py
@@ -1,59 +1,131 @@
 import requests
 import json
+from datetime import datetime
+from statistics import median
+# import matplotlib
 
-def get_github_pull_requests(owner, repo, token, page=1):
-    url = f"https://api.github.com/repos/{owner}/{repo}/pulls"
-    headers = {
-        "Authorization": f"token {token}",
-        "Accept": "application/vnd.github.v3+json"
-    }
-    params = {
-        "state": "closed",
-        "page": page,
-        "per_page": 100
-    }
-    response = requests.get(url, headers=headers, params=params)
-    response.raise_for_status()  # Raise an exception for bad status codes
-    return response.json()
+
+def get_github_pull_requests(owner, repo, token, page=1, number=None):
+   try:
+       url = f"https://api.github.com/repos/{owner}/{repo}/pulls"
+       if number:
+           url += f"/{number}"
+       headers = {
+           "Authorization": f"token {token}",
+           "Accept": "application/vnd.github.v3+json"
+       }
+       params = {
+           "state": "closed",
+           "page": page,
+           "per_page": 100
+       }
+       response = requests.get(url, headers=headers, params=params)
+       response.raise_for_status()  # Raise an exception for bad status codes
+       return response.json()
+   except requests.RequestException:
+       return get_github_pull_requests(owner, repo, token, page, number)
 
 
 def get_all_pages(owner, repo, token):
-    all_prs = []
-    page = 1
-    while True:
-        prs = get_github_pull_requests(owner, repo, token, page)
-        print(page)
-        if not prs:
-            break
-        all_prs.extend(prs)
-        page += 1
-    return all_prs
+   all_prs = []
+   page = 1
+   while True:
+       prs = get_github_pull_requests(owner, repo, token, page)
+       print(page)
+       if not prs:
+           break
+       all_prs.extend(prs)
+       page += 1
+   return all_prs
 
 
 def write_to_json_file(data, filename):
-    with open(filename, 'w') as f:
-        f.write(json.dumps(data, indent=4))
+   with open(filename, 'w') as f:
+       f.write(json.dumps(data, indent=4))
+
 
 def get_useful_info(pr):
-    return {
-        "id": pr["id"],
-        "title": pr["title"],
-        "created_at": pr["created_at"],
-        "closed_at": pr["closed_at"],
-        "merged_at": pr.get("merged_at"),
-    }
+   return {
+       "number": pr.get("number"),
+       "title": pr["title"],
+       "created_at": pr["created_at"],
+       "closed_at": pr["closed_at"],
+       "merged_at": pr.get("merged_at"),
+   }
 
 def move_through_prs(prs):
-    useful_prs = [get_useful_info(pr) for pr in prs]
-    return useful_prs
+   useful_prs = [get_useful_info(pr) for pr in prs]
+   return useful_prs
 
 
-files_names = {
-    "curl": "curl",
-}
-for name in files_names.keys():
-    data = get_all_pages(name, files_names[name], "YOUR_GITHUB_TOKEN_HERE")
-    print(len(data))
-    useful_data = move_through_prs(data)
-    file_name = name + ".json"
-    write_to_json_file(useful_data, file_name)
+def collect_and_write():
+   files_names = {
+       # "curl": "curl",
+       "zephyrproject-rtos": "zephyr",
+       "JabRef": "jabref",
+   }
+   for name in files_names.keys():
+       data = get_all_pages(name, files_names[name], "GH_token")
+       print(len(data))
+       useful_data = move_through_prs(data)
+       file_name = name + ".json"
+       write_to_json_file(useful_data, file_name)
+
+
+def read_json_file(filename):
+   with open(filename, "r") as f:
+       data = json.load(f)
+       return data
+
+
+def calculate_time_delta(created_at, closed_at):
+   format = '%Y-%m-%dT%H:%M:%SZ'
+   created_at_datetime = datetime.strptime(created_at, format)
+   closed_at_datetime = datetime.strptime(closed_at, format)
+   time_difference = closed_at_datetime - created_at_datetime
+   return time_difference.total_seconds() / 60
+
+
+def get_median_time_delta(data):
+   time_delta = []
+   for pr in data:
+       created_at = pr["created_at"]
+       closed_at = pr["closed_at"]
+       delta = calculate_time_delta(created_at, closed_at)
+       time_delta.append(delta)
+   return median(time_delta)
+
+
+def get_long_lived_prs(data, median_time_delta):
+   merged_pr_numbers = []
+   closed_pr_numbers = []
+   for pr in data:
+       created_at = pr["created_at"]
+       closed_at = pr["closed_at"]
+       delta = calculate_time_delta(created_at, closed_at)
+       number = pr["number"]
+       is_merged = pr["merged_at"] is not None
+       if delta > median_time_delta:
+           if is_merged:
+               merged_pr_numbers.append(number)
+           else:
+               closed_pr_numbers.append(number)
+
+
+   return closed_pr_numbers, merged_pr_numbers
+
+
+def make_histogram(data, median_time_delta):
+   return None
+
+
+collect_and_write()
+# data = read_json_file("cs480-group6/curl.json")
+# median_time_delta = get_median_time_delta(data)
+# make_histogram(data, median_time_delta)
+# closed_pr_numbers, merged_pr_numbers = get_long_lived_prs(data, median_time_delta)
+# print(len(closed_pr_numbers))
+# print(len(merged_pr_numbers))
+# print(merged_pr_numbers[44])
+# pr = get_github_pull_requests("curl", "curl", "GH_token", 1, "5218")
+# print(pr)
